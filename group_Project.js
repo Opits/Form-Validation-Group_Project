0811bec405a9b57e9313b5151ff28aa579f8e39c
Dubiously <123091137+Dubiously@users.noreply.github.com>
2023-08-09 21:41:48 +0200

    Add files via upload

--- group_Project.js
+++ group_Project.js
@@ -1,22 +1,13 @@
 // Global variables
+//form submission content element
+let formContent = document.getElementById('theForm');
+//field and label elements
 const cityField = document.getElementById('City');
 const cityLabel = document.getElementById('cityLabel');
 const countryField = document.getElementById('Country');
 const countryLabel = document.getElementById('countryLabel');
 const emailField = document.getElementById('Email');
 const emailLabel = document.getElementById('emailLabel');
-//error messages
-const errorMessage01 = document.getElementById('error-message01');
-const errorMessage02 = document.getElementById('error-message02');
-const errorMessage03 = document.getElementById('error-message03');
-// Define more errorMessageX variables as needed
-const errorMessage04 = document.getElementById('error-message04');
-const errorMessage05 = document.getElementById('error-message05');
-const errorMessage06 = document.getElementById('error-message06');
-const errorMessage07 = document.getElementById('error-message07');
-const errorMessage08 = document.getElementById('error-message08');
-const errorMessage09 = document.getElementById('error-message09');
-
 const form = document.getElementById('theForm');
 const idField = document.getElementById('userID');
 const idLabel = document.getElementById('userIDLabel');
@@ -30,6 +21,20 @@ const stateField = document.getElementById('State');
 const stateLabel = document.getElementById('stateLabel');
 const surnameField = document.getElementById('Last');
 const surnameLabel = document.getElementById('lastLabel');
+//error messages
+const errorMessage01 = document.getElementById('error-message01');
+const errorMessage02 = document.getElementById('error-message02');
+const errorMessage03 = document.getElementById('error-message03');
+const errorMessage04 = document.getElementById('error-message04');
+const errorMessage05 = document.getElementById('error-message05');
+const errorMessage06 = document.getElementById('error-message06');
+const errorMessage07 = document.getElementById('error-message07');
+const errorMessage08 = document.getElementById('error-message08');
+const errorMessage09 = document.getElementById('error-message09');
+//dropdown
+const countryDropdown = document.getElementById("Country");
+const cityDropdown = document.getElementById("City");
+const stateDropdown = document.getElementById("State");
 //icons
 const unValidCross = document.getElementById('cross');
 const validTick = document.getElementById('tick');
@@ -37,9 +42,50 @@ const validTick = document.getElementById('tick');
 let showErrorMessage = false;
 let formSubmitted = false;
 
-//let errorMessage = document.getElementById('error-message');
-//variables end
-////////////////////////////////////////////////////////////////////////////////////////
+//arrays and obj
+//array of cities to their respective countries
+const citiesByCountry = {
+  "": ["-- Select a Country --"],
+  US: ["-- Select a City --", "New York", "Los Angeles", "Chicago"],
+  Canada: ["-- Select a City --", "Toronto", "Vancouver", "Montreal"],
+  ZA: ["-- Select a City --", "Johannesburg", "Cape Town", "Pretoria", "Durban"],
+  India: ["-- Select a City --", "Mumbai", "Delhi", "Bangalore", "Pune"],
+  UK: ["-- Select a City --", "London", "Edinburgh", "Belfast"],
+};
+//array of states to their respective countries
+const statesByCountry = {
+  "": ["-- Select a Country --"],
+  US: ["-- Select a State --", "New York", "California", "Illinois"],
+  Canada: ["-- Select a State --", "Ontario", "British Columbia", "Quebec"],
+  India: ["-- Select a State --", "Maharashtra", "Delhi", "Karnataka"],
+  UK: ["-- Select a State --", "Scotland", "Northern Ireland", "England"],
+  ZA: ["-- Select a Province --", "Gauteng", "Eastern Cape", "Western Cape", "Free State", "Kwa-Zulu Natal",],
+};
+//field value constraints
+const fields = [
+  { input: nameField, error: errorMessage01, validation: /^[A-Za-z]+$/, errorMsg: 'Name may only contain letters' },
+  { input: surnameField, error: errorMessage02, validation: /^[A-Za-z ]+$/, errorMsg: 'Last Name may only contain letters' },
+  { input: phoneInput, error: errorMessage03, validation: /^\d{10}$/, errorMsg: 'Please enter a valid phone number!' },
+  // Add more fields here with their corresponding error messages and validation patterns
+  { input: emailField, error: errorMessage04, validation: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, errorMsg: 'Please enter a valid email address!' },
+  { input: idField, error: errorMessage05, validation: /.+/, errorMsg: 'Please enter a valid ID!' },
+  { input: countryField, error: errorMessage06, validation: /.+/, errorMsg: 'Please select a country!' },
+  { input: stateField, error: errorMessage07, validation: /.+/, errorMsg: 'Please enter a state!' },
+  { input: cityField, error: errorMessage08, validation: /.+/, errorMsg: 'Please enter a city!' },
+  { input: referenceField, error: errorMessage09, validation: /.+/, errorMsg: 'Please enter a reference code!' }
+];
+//user values for their submitted form 
+const userData = {
+  firstName: /^[A-Za-z]+$/,
+  lastName: /^[A-Za-z ]+$/,
+  email: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/,
+  userID: /.+/,
+  country: /.+/,
+  state: /.+/,
+  city: /.+/,
+  phoneNumber: /^\d{10}$/,
+  referenceCode: /.+/
+};
 
 //all fields for highlight function
 nameField.addEventListener('focus', highlightClick);
@@ -54,130 +100,60 @@ referenceField.addEventListener('focus', highlightClick);
 phoneInput.addEventListener('focus', highlightClick);
 //highlight a field when clicked on
 function highlightClick(e) {
-    try {
-        const targetId = e.target.id;
-        const inputElement = document.getElementById(targetId);
-        const labelElement = document.querySelector(`label[for="${targetId}"]`);
+  try {
+    const targetId = e.target.id;
+    const inputElement = document.getElementById(targetId);
+    const labelElement = document.querySelector(`label[for="${targetId}"]`);
 
-        const elementsToClear = document.querySelectorAll('.highlightField, .highlightHead');
-        elementsToClear.forEach(element => {
-            element.classList.remove('highlightField', 'highlightHead');
-        });
+    const elementsToClear = document.querySelectorAll('.highlightField, .highlightHead');
+    elementsToClear.forEach(element => {
+      element.classList.remove('highlightField', 'highlightHead');
+    });
 
-        if (inputElement) {
-            inputElement.classList.add('highlightField');
-        }
-        if (labelElement) {
-            labelElement.classList.add('highlightHead');
-        }
-    } catch (err) {
-        console.log(err);
+    if (inputElement) {
+      inputElement.classList.add('highlightField');
     }
+    if (labelElement) {
+      labelElement.classList.add('highlightHead');
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-//end of highlight
-////////////////////////////////////////////////////////////////////////////////////////
-
-const fields = [
-    { input: nameField, error: errorMessage01, validation: /^[A-Za-z]+$/, errorMsg: 'Name may only contain letters' },
-    { input: surnameField, error: errorMessage02, validation: /^[A-Za-z]+$/, errorMsg: 'Last Name may only contain letters' },
-    { input: phoneInput, error: errorMessage03, validation: /^\d{10}$/, errorMsg: 'Please enter a valid phone number!' },
-    // Add more fields here with their corresponding error messages and validation patterns
-    { input: emailField, error: errorMessage04, validation: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, errorMsg: 'Please enter a valid email address!' },
-    { input: idField, error: errorMessage05, validation: /.+/, errorMsg: 'Please enter a valid ID!' },
-    { input: countryField, error: errorMessage06, validation: /.+/, errorMsg: 'Please select a country!' },
-    { input: stateField, error: errorMessage07, validation: /.+/, errorMsg: 'Please enter a state!' },
-    { input: cityField, error: errorMessage08, validation: /.+/, errorMsg: 'Please enter a city!' },
-    { input: referenceField, error: errorMessage09, validation: /.+/, errorMsg: 'Please enter a reference code!' }
-];
-
-
-// Submit event listener for the form
+// event listener for the form Submit button
 form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    formSubmitted = true;
 
-    fields.forEach(field => {
-        const isValid = field.validation.test(field.input.value);
-        if (!isValid) {
-            field.error.textContent = field.errorMsg;
-        } else {
-            field.error.textContent = '';
-        }
-    });
-});
+  event.preventDefault();
 
+  formSubmitted = true;
+  fields.forEach(field => {
+    const isValid = field.validation.test(field.input.value);
+    if (!isValid) {
+      field.error.textContent = field.errorMsg;
+      formSubmitted = false;
+    } else {
+      field.error.textContent = '';
+    }
+  });
 
+  if (formSubmitted == true) {
+    userData.firstName = document.getElementById("First").value;
+    userData.lastName = document.getElementById("Last").value;
+    userData.email = document.getElementById("Email").value;
+    userData.userID = document.getElementById("userID").value;
+    userData.country = document.getElementById("Country").value;
+    userData.state = document.getElementById("State").value;
+    userData.city = document.getElementById("City").value;
+    userData.phoneNumber = document.getElementById("Phone").value;
+    userData.referenceCode = document.getElementById("RefCod").value;
 
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    // Get input values
-    const firstName = document.getElementById("First").value;
-    const lastName = document.getElementById("Last").value;
-    const email = document.getElementById("Email").value;
-    const userID = document.getElementById("userID").value;
-    const country = document.getElementById("Country").value;
-    const state = document.getElementById("State").value;
-    const city = document.getElementById("City").value;
-    const phoneNumber = document.getElementById("Phone").value;
-    const referenceCode = document.getElementById("RefCod").value;
-
-    // Store values in variables
-    const userData = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        userID: userID,
-        country: country,
-        state: state,
-        city: city,
-        phoneNumber: phoneNumber,
-        referenceCode: referenceCode
-    };
-
-    // Do something with the userData object (e.g., send it to a server)
     console.log(userData);
-});
-
-//If the email-filed includes an "@" it will display a green tick, else it will display a red cross.
-emailField.addEventListener('input', function () 
-{
-  if (emailField.value.includes('@'))
-  {
-    validTick.style.display = 'inline';
-    unValidCross.style.display = 'none';
-  }
-  else
-  {
-    validTick.style.display = 'none';
-    unValidCross.style.display = 'inline';
+    loadNewPage();
   }
 });
-
-
-const countryDropdown = document.getElementById("Country");
-const cityDropdown = document.getElementById("City");
-const stateDropdown = document.getElementById("State");
-
-const citiesByCountry = {
-  "":["-- Select a Country --"],
-  US: ["-- Select a City --","New York", "Los Angeles", "Chicago"],
-  Canada: ["-- Select a City --","Toronto", "Vancouver", "Montreal"],
-  ZA: ["-- Select a City --","Johannesburg","Cape Town", "Pretoria","Durban" ],
-  India: ["-- Select a City --","Mumbai", "Delhi", "Bangalore", "Pune"],
-  UK: ["-- Select a City --","London", "Edinburgh", "Belfast"],
-};
-
-const statesByCountry = {
-  "":["-- Select a Country --"],
-  US: ["-- Select a State --","New York", "California", "Illinois"],
-  Canada: ["-- Select a State --","Ontario", "British Columbia", "Quebec"],
-  India: ["-- Select a State --","Maharashtra", "Delhi", "Karnataka"],
-  UK: ["-- Select a State --", "Scotland", "Northern Ireland", "England"],
-  ZA: ["-- Select a Province --","Gauteng","Eastern Cape", "Western Cape","Free State","Kwa-Zulu Natal", ],
-};
-
+//dynamically link the countries to their respective states and cities
+populateCitiesAndStates();
 function populateCitiesAndStates() {
   const selectedCountry = countryDropdown.value;
   const cities = citiesByCountry[selectedCountry];
@@ -205,4 +181,87 @@ function populateCitiesAndStates() {
   }
 }
 
-populateCitiesAndStates();
+//If the email-filed includes an "@" it will display a green tick, else it will display a red cross.
+emailField.addEventListener('input', function () {
+  if (emailField.value.includes('@')) {
+    validTick.style.display = 'inline';
+    unValidCross.style.display = 'none';
+  }
+  else {
+    validTick.style.display = 'none';
+    unValidCross.style.display = 'inline';
+  }
+});
+
+//dynamically changes the page to show the user their input field values
+function loadNewPage() {
+  formContent = document.getElementById('theForm');
+  // Hides the original content
+  formContent.classList.add('hidden');
+
+  // Creates the new content for the new page
+  const newContent = document.createElement('div');  
+  newContent.id = 'newContent'
+  newContent.innerHTML = `
+        <p>This is the new content.</p>
+        <list>
+          <ul>
+            <li id="name">name: </li>
+            <li id="surname">surname: </li>
+            <li id="email">email: </li>
+            <li id="id">id: </li>
+            <li id="country">country: </li>
+            <li id="state">state: </li>
+            <li id="city">city: </li>
+            <li id="phone">phone: </li>
+            <li id="ref">ref: </li>
+          </ul>
+        </list>
+        <a href="#" id="goBack">Go Back</a>
+    `;
+
+  // Append the new content
+  document.body.appendChild(newContent);
+
+  // Attach an event listener to the "Go Back" link to revert to the original page
+  document.getElementById('goBack').addEventListener('click', loadOriginalPage);
+
+  // Append the user data values to the list items
+  document.querySelector('#name').textContent += userData.firstName;
+  document.querySelector('#surname').textContent += userData.lastName;
+  document.querySelector('#email').textContent += userData.email;
+  document.querySelector('#id').textContent += userData.userID;
+  document.querySelector('#country').textContent += userData.country;
+  document.querySelector('#state').textContent += userData.state;
+  document.querySelector('#city').textContent += userData.city;
+  document.querySelector('#phone').textContent += userData.phoneNumber;
+  document.querySelector('#ref').textContent += userData.referenceCode;
+}
+//removes the page that show the user his data
+function loadOriginalPage() {
+  // Shows original content
+  formContent.classList.remove('hidden');
+
+  // Removes new content
+  const newContent = document.getElementById('newContent');
+  if (newContent) {
+    newContent.remove();
+  }
+
+  reset();
+}
+
+//reset button event listener that trigger the function reset
+form.addEventListener("reset", reset);
+
+//a function to rest the input fields
+function reset(event) {
+  formSubmitted = false;
+  validTick.style.display = 'none';
+  unValidCross.style.display = 'none';
+
+  fields.forEach(field => {
+    field.input.value = ''; // Clear the input field value
+    field.error.textContent = '';
+  });
+}
